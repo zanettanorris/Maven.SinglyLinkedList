93df81e1d07ed66ff685cb121e4468ef8acfba88
Zanetta Norris <zanettanorris@gmail.com>
2020-03-28 01:51:28 -0400

    added code and tests

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,147 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import java.lang.reflect.Array;
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<T> {
+    private Node head;
+    private int listSize = 0;
+
+    private class Node {
+        private T element;
+        private Node next;
+
+        //constructor with just generic element
+        public Node(T element) {
+            this.element = element;
+        }
+
+        //constructor with next node and generic element
+        public Node(Node next, T element) {
+            this.element = element;
+            this.next = next;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+        public T getElement() {
+            return element;
+        }
+
+    }
+
+    private void incrementListSize() {
+        listSize++;
+    }
+
+    private void decrementListSize() {
+        listSize--;
+    }
+
+    private int getListSize() {
+        return listSize;
+    }
+
+    //adds element to end of list
+
+    public void addElement(T element) {
+        //if list is empty, add element to head of list
+        Node newNode = new Node(element);
+        if (this.head == null) {
+            this.head = newNode;
+        }
+        //else walk to the last node  and insert the new node there
+        else {
+            Node last = head;
+            while (last.getNext() != null) {
+                last = last.getNext();
+            } //this bracket was in the wrong place.
+            last.setNext(newNode);
+        }
+        incrementListSize();
+    }
+        //} it was over here.
+
+    public boolean removeElementByIndex(int index) {
+        //if the index is out of range exit
+        if (index < 0 || index > getListSize() || head == null)
+            return false;
+        if (index == 0) {
+            head = head.getNext();
+            return true;
+        }
+        //find element at index
+        Node previous = null;
+        Node current = head;
+        for (int listIndex = 1; listIndex < index; listIndex++) {
+            previous = current;
+            current = current.getNext();
+        }
+        previous.setNext(current.getNext());
+        decrementListSize();
+        return true;
+    }
+
+    public boolean containsElement(T element) {
+        //if list contains element return true if not return false
+        if (head == null) {
+            return false;
+        }
+        Node selectedElement = head;
+        if (selectedElement.getElement().equals(element)) {
+            return true;
+        }
+        for (int i = 0; i < getListSize(); i++) {
+            selectedElement = selectedElement.getNext();
+            if (selectedElement.getElement().equals(element)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int findIndexByElement(T element) {
+        // param is element return index in list of element
+        // or -1 if element is not in list
+        if (head == null) {
+            return -1;
+        }
+        Node selectedNode = head;
+        for (int i = 0; i < getListSize(); i++) {
+            if (selectedNode.getElement().equals(element)) {
+                return i;
+            }
+            selectedNode = selectedNode.getNext();
+        }
+        return -1;
+    }
+
+    public Integer getSizeOfList() {
+        return getListSize();
+    }
+
+    public T getElementByIndex(int index) {
+        if (index < 0 || index > getListSize())
+            return null;
+        Node selectedElement = head;
+        for (int i = 0; i < index; i++) {
+            selectedElement = selectedElement.getNext();
+        }
+        return selectedElement.getElement();
+    }
+
+
+    public SinglyLinkedList<T> copySameValues() {
+        return null;
+    }
+
+    public void sortList() {
+    }
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,87 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+    private static String ELEMENT1 = "ELEMENT1";
+    private static String ELEMENT2 = "ELEMENT2";
+    private static String ELEMENT3 = "ELEMENT3";
+
+    private SinglyLinkedList<String>list;
+    @Before
+    public void setup() {
+        list = new SinglyLinkedList<String>();
+        Assert.assertTrue(list.getSizeOfList() == 0);
+    }
+
+    @Test
+    public void addElementTest(){
+        list.addElement(ELEMENT1);
+        Assert.assertTrue(list.getSizeOfList()==1);
+        String element = list.getElementByIndex(0);
+        Assert.assertEquals(ELEMENT1, element);
+        list.addElement(ELEMENT2);
+        Assert.assertTrue(list.getSizeOfList()==2);
+    }
+
+    @Test
+    public void removeElementByIndexTest() {
+        Assert.assertTrue(list.getSizeOfList() == 0);
+        //index less than zero
+        boolean removeSuccess = list.removeElementByIndex(-1);
+        Assert.assertFalse(removeSuccess);
+        //Index greater than size of array
+        removeSuccess = list.removeElementByIndex(1000);
+        Assert.assertFalse(removeSuccess);
+        //Head does not exist
+        removeSuccess = list.removeElementByIndex(0);
+        Assert.assertFalse(removeSuccess);
+
+        list.addElement(ELEMENT1);
+        list.addElement(ELEMENT2);
+        list.addElement(ELEMENT3);
+        Assert.assertTrue(list.getSizeOfList() == 3);
+
+        list.removeElementByIndex(2);
+        Assert.assertTrue(list.getSizeOfList() == 2);
+        String elementAt1 = list.getElementByIndex(1);
+        Assert.assertEquals(ELEMENT3, elementAt1);
+    }
+
+    @Test
+    public void containsElementTest() {
+        Assert.assertFalse(list.containsElement(ELEMENT1));
+        list.addElement(ELEMENT1);
+        Assert.assertTrue(list.getSizeOfList() == 1);
+        Assert.assertTrue(list.containsElement(ELEMENT1));
+        list.addElement(ELEMENT2);
+        Assert.assertTrue(list.getSizeOfList() == 2);
+        Assert.assertTrue(list.containsElement(ELEMENT2));
+    }
+
+    @Test
+    public void findElementByIndexTest() {
+        list.addElement(ELEMENT1);
+        Assert.assertTrue(list.containsElement(ELEMENT1));
+        Assert.assertTrue(list.findIndexByElement(ELEMENT1) == 0);
+        list.addElement(ELEMENT2);
+        Assert.assertTrue(list.containsElement(ELEMENT2));
+        Assert.assertTrue(list.findIndexByElement(ELEMENT2) == 1);
+    }
+
+    @Test
+    public void getElementByIndex() {
+        list.addElement(ELEMENT1);
+        String element = list.getElementByIndex(0);
+        Assert.assertEquals(ELEMENT1, element);
+        list.addElement(ELEMENT2);
+        String element2 = list.getElementByIndex(1);
+        Assert.assertEquals(ELEMENT2, element2);
+    }
+
 }
